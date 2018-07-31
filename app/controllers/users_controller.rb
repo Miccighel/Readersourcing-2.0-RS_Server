e98773bbafe2fcaae7bd59b99e6fe34719f30582
Miccighel <miccighel@gmail.com>
2018-07-31 16:20:35 +0200

    UsersController prepared to allow create operation, model updated with some validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  skip_before_action :authenticate_request, only: :create
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users.json
@@ -14,7 +15,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     if @user.save
       render :show, status: :created, location: @user
     else
@@ -37,13 +37,12 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:firstName, :lastName, :email)
+      params.require(:user).permit(:first_name, :last_name, :email, :orcid, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
 	has_many :ratings, dependent: :destroy
 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 	has_secure_password
 
 end
