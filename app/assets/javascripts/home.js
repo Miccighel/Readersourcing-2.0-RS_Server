4dad7be543990c7fd175fdaa38a8ce757e17e1de
Miccighel <miccighel@gmail.com>
2019-01-08 17:01:02 +0100

    fixes to the previous commit

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,6 +5,7 @@
 let loginButton = $("#login-btn");
 let signUpButton = $("#sign-up-btn");
 
+let signInIcon = $("#sign-in-icon");
 let signUpIcon = $("#sign-up-icon");
 let reloadIcons = $(".reload-icon");
 
@@ -22,6 +23,7 @@ if (authToken != null) {
 //######### GO TO LOGIN HANDLING #########//
 
 loginButton.on("click", () => {
+	loginButton.find(signInIcon).toggle();
 	loginButton.find(reloadIcons).toggle();
 });
 
