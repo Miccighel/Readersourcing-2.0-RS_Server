d879fd745ec5934f339299d7c097b8c76505e07d
Miccighel <miccighel@gmail.com>
2019-01-21 16:52:37 +0100

    minor fixes

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,9 +1,6 @@
-// This code set a fake preloader for every page. The JS code responsible for each of them must remove it when the initialization is completed.
+////////// INIT  //////////
 
-$(window).on("load", () => {
-	let body = $('body');
-	body.addClass('preloader');
-});
+//######## CLIENT SIDE VALIDATION SETUP ########//
 
 Parsley.addMessages('it', {
 	defaultMessage: "Questo valore sembra essere non valido.",
@@ -68,3 +65,14 @@ Parsley.addValidator('password', {
 Parsley.setLocale('en');
 
 Dropzone.autoDiscover = false;
+
+////////// GLOBAL ///////////
+
+//######### PAGE LOADING HANDLING #########//
+
+// This code set a fake preloader for every page. The JS code responsible for each of them must remove it when the initialization is completed.
+
+$(window).on("load", () => {
+	let body = $('body');
+	body.addClass('preloader');
+});
